2aa850bec75127cd393509216191c923e856f2f5
Cameron Cummings <cameron.cummings@tecuity.com>
2019-05-04 17:32:38 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,14 +30,21 @@ const EditExpensePage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        Yo, this page is not found 404, 404, 404, 404...
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/help" component={HelpPage} />
             <Route path="/edit" component={EditExpensePage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
