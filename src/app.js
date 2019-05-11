c61838f8f0d9efef10033b27a877239a68082357
Cameron Cummings <cameron.cummings@tecuity.com>
2019-05-10 18:09:21 -0600

    Linking between routes - Link and Navlink

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -32,19 +32,33 @@ const EditExpensePage = () => (
 
 const NotFoundPage = () => (
     <div>
-        Yo, this page is not found 404, 404, 404, 404...
+        404 - This is not the page you are looking for...<Link to="/">Go Home</Link>
     </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/expense/new" activeClassName="is-active">New Expense</NavLink>
+        <NavLink to="/expense/{expense-id}/edit" activeClassName="is-active">Edit Expense</NavLink>
+        <NavLink to="/help-page" activeClassName="is-active">Help</NavLink>
+    </header>
 );
 
 const routes = (
     <BrowserRouter>
-        <Switch>
+        <div>
+            <Header />
+            <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/expense/new" component={AddExpensePage} />
+            <Route path="/help-page" component={HelpPage} />
+            <Route path="/expense/{expense-id}/edit" component={EditExpensePage} />
             <Route component={NotFoundPage} />
         </Switch>
+        </div>
+
     </BrowserRouter>
 );
 
